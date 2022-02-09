cad02c5baaaf0f1e7e5037606909d2668971ffd9
“guymargalith” <guymargalith@gmail.com>
2022-02-09 16:40:17 +0000

    added more tests and fixed edit profile

--- frontend/src/tests/CompetitionLeaderboard.test.js
+++ frontend/src/tests/CompetitionLeaderboard.test.js
@@ -3,21 +3,27 @@
  */
 
 import React from "react";
-import { screen, render } from "@testing-library/react";
-import { Provider } from "react-redux";
+import { screen, render, fireEvent } from "@testing-library/react";
+import { Provider} from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 
 import { CompetitionLeaderboard } from "../pages";
 import { store } from "../redux/store/store";
 
 describe("CompetitionLeaderboard", () => {
+  const redux = require('react-redux')
   let mockFunction;
   global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve([{name: "test name", description: "test desc", units: "test units", frquency:1}])
+    json: () => Promise.resolve({name: "test name", description: "test desc", units: "test units", frquency:1, scores: [{id:1,user_id:1, last_updated: "2022-04-01", user: {username: "test", }}]}),
+    ok: true,
   })
 );
-  beforeEach(() => {
+  beforeEach(async() => {
+    jest.spyOn(localStorage, "getItem");
+    localStorage.getItem = jest.fn();
+    jest.spyOn(redux, 'useSelector');
+    redux.useSelector = jest.fn().mockReturnValue(true);
     // mockFunction=jest.fn().mockReturnValue([{name: "test name", description: "test desc", units: "test units", frquency:1}])
     fetch.mockClear();
     render(
@@ -32,4 +38,10 @@ describe("CompetitionLeaderboard", () => {
     const leaderboard = screen.getByLabelText("leaderboard");
     expect(leaderboard).toBeTruthy();
   });
+
+  test("updates score", () => {
+    const form = screen.getByLabelText("update-score")
+    fireEvent.submit(form, {preventDefault: jest.fn()})
+    expect(form).toBeInTheDocument()
+  })
 });
